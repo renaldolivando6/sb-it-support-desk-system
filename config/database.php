190cb2c75d90cb50fa29f1b6e7d96299ed047bb9
Renaldo Livando <renaldolivando6@gmail.com>
2026-03-25 16:42:28 +0700

    add db mysql

--- config/database.php
+++ config/database.php
@@ -21,6 +21,18 @@ return [
         //PDO::ATTR_PERSISTENT => true
         ],
     ],
+
+    'mysql' => [
+        'driver' => 'mysql',
+        'host' => env('DB_HOST', '127.0.0.1'),
+        'port' => env('DB_PORT', '3306'),
+        'database' => env('DB_DATABASE', 'internalsupportsb'),
+        'username' => env('DB_USERNAME', 'root'),
+        'password' => env('DB_PASSWORD', ''),
+        'charset' => 'utf8mb4',
+        'collation' => 'utf8mb4_unicode_ci',
+        'prefix' => '',
+    ],
     
     'sqlsrv_194' => [
         'driver' => 'sqlsrv',
